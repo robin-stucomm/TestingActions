25893fef181f62022ee4ca21ff97b567ad100da6
Robin de Jong <robin@stucomm.com>
2020-11-09 16:23:52 +0100

    More testing

--- swagger/upload.sh
+++ swagger/upload.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-
-echo "Hello World!?"
-ls
+swagger=("private" "public")
+projectVersion=$(./gradlew properties --no-daemon --console=plain -q | grep "^version:" | awk '{printf $2}')
+echo "Generating the public swagger definition"
+# ./gradlew generatePublic -q
+echo "Generating the private swagger definition"
+# ./gradlew generatePrivate -q
+echo "Uploading versions: $projectVersion"
+for job in "${swagger[@]}"; do
+    tag="swagger-$job:$projectVersion"
+    echo "$tag"
+done
